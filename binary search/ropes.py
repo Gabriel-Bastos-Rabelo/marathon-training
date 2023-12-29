97e90ae6ebb000a855bc3247956379def96656a9
Gabriel <gbastosrabelo@gmail.com>
2023-12-29 20:45:06 -0300

    adding ropes question

--- binary search/ropes.py
+++ binary search/ropes.py
@@ -4,9 +4,27 @@ ropes = []
 for i in range(n):
     ropes.append(int(input()))
 
+maiorCorda = max(ropes)
 
 def isPossible(ropes, k, tamanho):
     partes = 0
     for rope in ropes:
         if tamanho <= rope:
             partes += rope//tamanho
+
+
+    return partes >= k
+
+
+
+l = 0
+r = maiorCorda
+
+while(l <= r):
+    mid = l + (r - l)/2
+    if(isPossible(ropes, k, mid)):
+        l = mid + 0.0000001
+    else:
+        r = mid - 0.0000001
+
+print(f"{l:.2f}")
