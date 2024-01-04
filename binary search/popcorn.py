72681dc30a704a923e1c2e01419de33162f8ae41
Gabriel <gbastosrabelo@gmail.com>
2024-01-04 18:23:49 -0300

    adding beecrowd question

--- binary search/popcorn.py
+++ binary search/popcorn.py
@@ -1,11 +1,52 @@
+#beecrowd 2973
+
 n, c, t = map(int, input().split())
 
 
-l = list(map(int, input().split()))
-
-p = sum(l)
+pipocas = list(map(int, input().split()))
 
 
-def isPossible():
+
+
+def isPossible(tempo, pipocas, max, p):
+    r = tempo*max
+    n = 1
+    i = 0
+
+    while i < len(pipocas):
+        if pipocas[i] > tempo * max:
+            return False
+        if r >= pipocas[i]:
+            r -= pipocas[i]
+
+        else:
+            n += 1
+            r = tempo * max
+            i -= 1
+            if(n > p):
+                return False
+        
+        i += 1
+
+
+
+    return True
+
+
+
+l = 0
+r = 10 ** 9
+
+while(l <= r):
+    mid = l + (r - l)//2
+    if(isPossible(mid, pipocas, t, c)):
+        r = mid - 1
+    
+    else:
+        l = mid + 1
+
+print(l)
+
+
    
 
